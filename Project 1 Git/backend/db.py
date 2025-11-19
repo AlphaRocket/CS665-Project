a8180719556c86d53c66b7608c362e4cc117d4c3
AlphaRocket <jxdog2005@gmail.com>
2025-11-18 23:04:36 -0600

    Add PostgreSQL connection in db.py using dotenv

---
+++ Project 1 Git/backend/db.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    dbname=os.getenv("PGDATABASE"),
+    user=os.getenv("PGUSER"),
+    password=os.getenv("PGPASSWORD"),
+    host=os.getenv("PGHOST"),
+    port=os.getenv("PGPORT")
+)
+
+cursor = conn.cursor()
+print("Connected to PostgreSQL")
